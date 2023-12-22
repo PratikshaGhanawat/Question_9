cd3102223a94a0b9eefc45e619123d2db45484ff
PratikshaGhanawat <94125522+PratikshaGhanawat@users.noreply.github.com>
2023-12-22 22:35:29 +0530

    Add files via upload

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Your server-side code
+
+            // Endpoint to handle AJAX request
+            if (Request.QueryString["action"] == "loadData")
+            {
+                // Simulate loading data from the server (replace with your logic)
+                string loadedData = "Data loaded from the server.";
+
+                // Return the loaded data
+                Response.Write(loadedData);
+                Response.End();
+            }
+        }
+
+        protected void btnShowModal_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
